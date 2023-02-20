616a88a0159685d8d01b98f6c2d08cf344ea8047
Sahil Adhav <sahiladhav@Sahils-MacBook-Pro.local>
2023-02-20 19:25:19 +0530

    mongoDB issue

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 const posts = [];
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser : true,
+});
+
+const postSchema = {
+  title : String,
+  content : String
+};
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,19 +21,21 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+//creating a new mongoose model
+
+const Post = mongoose.model("Post", postSchema);
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req,res) => {
-  res.render("home");
-});
-
-app.get("/home", (req, res) => {
-  res.render("home", {
-    homeStartingContent : homeStartingContent,
-    posts : posts 
+  Post.find({}, (err, posts) => {
+    res.render("home", {
+      homeStartingContent : homeStartingContent,
+      posts : posts
+    });
   });
 });
 
@@ -59,12 +70,16 @@ app.get("/posts/:postName", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const blog = {
-    blogTitle : req.body.blogTitle,
-    blogContent : req.body.blogContent
-  }
-  posts.push(blog);
-  res.redirect("/home");
+  const post = new Post({
+    title : req.body.blogTitle,
+    content : req.body.blogContent
+  });
+
+  post.save((err) => {
+    if(!err){
+      res.redirect("/");
+    }
+  });
 });
 
 app.listen(3000, function() {
